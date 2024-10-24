d10579c8465b570e822e89c5a519cd1fdc270c8a
ramesh <ramesh>
2024-10-24 11:49:42 +0530

    update

--- deploy/image-updater.sh
+++ deploy/image-updater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the file to modify
-FILE="deployment.yaml"
+FILE="/var/lib/jenkins/workspace/portfolio-ramesh/deploy/deployment.yaml" # Update path if needed
 
 # Replace UPDATE-IMAGE with the new image tag
 sed -i "s|UPDATE-IMAGE|rameshxt/portfolio-ramesh:v1.0.0.${BUILD_NUMBER}|g" "$FILE"
