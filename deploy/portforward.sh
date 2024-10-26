15f3bf6ccf27179bd0042fc09e8b6fe256507fd1
ramesh <ramesh>
2024-10-26 20:22:55 +0530

    porforward added

--- deploy/portforward.sh
+++ deploy/portforward.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Port Forward
-kubectl port-forward service/portfolio-service 30050 --address 0.0.0.0 &
+kubectl port-forward service/portfolio-service 30050:80 --address 0.0.0.0 &
 
 # finish
-echo "port forwading"
+echo "port forwarding"
