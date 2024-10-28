eac5310dc2f9627fe1eb29b916338f638c73e5b1
ramesh <ramesh>
2024-10-28 10:30:59 +0530

    ip fetch added in app

--- deploy/app.sh
+++ deploy/app.sh
@@ -198,6 +198,11 @@ FILE_PATH="/home/ec2-user/portforward.sh"
 # Define the content to be written
 CONTENT='#!/bin/bash
 
+GREEN="\e[32m"
+BLUE="\e[34m"
+RED="\e[31m"
+NC="\e[0m"
+
 # Port Forwarding
 echo "Starting port forwarding..."
 kubectl port-forward service/portfolio-service 30050:80 --address 0.0.0.0 > port-forward.log 2>&1 &
@@ -207,11 +212,16 @@ sleep 5
 
 # Check if the port forwarding command is running
 if pgrep -f "kubectl port-forward" > /dev/null; then
-    echo "Port forwarding established on port 30050"
+    echo "${GREEN}Port forwarding established on port 30050${NC}"
+    
+    # Fetch the public IP of the instance
+    public_ip=$(curl -s ifconfig.co)
+
+    # Display the full access URL
+    echo "You can access your application at: ${BLUE}http://$public_ip:30050${NC}"
 else
-    echo "Failed to establish port forwarding. Check port-forward.log for errors."
-fi
-'
+    echo "${RED}Failed to establish port forwarding. Check port-forward.log for errors.${NC}"
+fi'
 
 # Create the file and write the content
 echo "$CONTENT" > "$FILE_PATH"
