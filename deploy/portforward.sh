24c5c47be91324ff014b8e189a64391855090807
ramesh <ramesh>
2024-11-09 11:57:05 +0530

    portforward modified

--- deploy/portforward.sh
+++ deploy/portforward.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 GREEN="\e[32m"
-BLUE="\e[34m"
+SKYBLUE="\e[32m"
 RED="\e[31m"
 NC="\e[0m"
 
-echo "Starting port forwarding..."
+echo -e "Starting port forwarding..."
 kubectl port-forward service/portfolio-service 30050:80 --address 0.0.0.0 > port-forward.log 2>&1 &
 
 sleep 5
 
 # Check if the port forwarding command is running
 if pgrep -f "kubectl port-forward" > /dev/null; then
-    echo "${GREEN}Port forwarding established on port 30050${NC}"
-    
-    public_ip=$(curl -s ifconfig.co)
+    echo -e "${GREEN}Port forwarding established on port 30050${NC}"
 
-    echo "You can access your application at: ${BLUE}http://$public_ip:30050${NC}"
+    public_ip=$(curl -s https://ipinfo.io/ip)
+
+    echo -e "You can access your application at: ${SKYBLUE}http://$public_ip:30050${NC}"
 else
-    echo "${RED}Failed to establish port forwarding. Check port-forward.log for errors.${NC}"
+    echo -e "${RED}Failed to establish port forwarding. Check port-forward.log for errors.${NC}"
 fi
